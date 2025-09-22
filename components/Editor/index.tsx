b95f7bb3366562658828c8625db0eac4e2ef0a7f
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-22 22:56:12 +0530

    basic layout

---
+++ components/Editor/index.tsx
@@ -0,0 +1,6 @@
+import styles from "./index.module.css";
+const Editor=()=>{
+    return <div className={styles.editor}>Editor Component</div>
+}
+
+export default Editor;
---
+++ components/OutputScreen/index.tsx
@@ -0,0 +1,6 @@
+import styles from "./index.module.css";
+const OutputScreen = () => {
+    return <div className={styles.outputScreen}>Output Screen Component</div>
+}
+
+export default OutputScreen;
