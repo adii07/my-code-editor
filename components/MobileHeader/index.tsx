77e2c6b6c6dc5eac8f0971d38e4393d2f7ed9076
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-23 01:19:45 +0530

    making side responsive

---
+++ components/MobileHeader/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./index.module.css";
+type MobileHeaderProps = {
+    selectedTab: "input" | "output";
+    setSelectedTab: (tab: "input" | "output") => void;
+}
+const MobileHeader = ({ selectedTab, setSelectedTab }: MobileHeaderProps) => {
+    return (
+        <div className={styles.mobile_header}>
+            <button className={`${styles.mobile_button} ${selectedTab === "input" ? styles.selected : ""}`} onClick={() => setSelectedTab("input")}>Input</button>
+            <button className={`${styles.mobile_button} ${selectedTab === "output" ? styles.selected : ""}`} onClick={() => setSelectedTab("output")}>Output</button>
+        </div>
+    );
+}
+
+export default MobileHeader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Editor from "../../components/Editor";
 import OutputScreen from "../../components/OutputScreen";
 import styles from "./page.module.css";
+import { useWindowSize } from "../../hooks/isDesktop";
+import MobileHeader from "../../components/MobileHeader";
 
 export default function Home() {
   const [output, setOutput] = useState("");
+  const { isDesktop } = useWindowSize();
+  const [selectedTab, setSelectedTab] = useState<"input" | "output">("input");
+
+  useEffect(() => { if (!isDesktop) setSelectedTab("output") }, [output]);
   return (
-    <div className={styles.page}>
-      <Editor setOutput={setOutput} />
-      <OutputScreen output={output} setOutput={setOutput} />
-    </div>
+    <>
+      {isDesktop ? <div className={styles.page}>
+        <Editor setOutput={setOutput} />
+        <OutputScreen output={output} setOutput={setOutput} />
+      </div> :
+        <div className={styles.mobile_page} >
+          <MobileHeader selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+          {selectedTab === "input" && <Editor setOutput={setOutput} />}
+          {selectedTab === "output" && <OutputScreen output={output} setOutput={setOutput} />}
+        </div>
+      }
+    </>
+
   );
 }
