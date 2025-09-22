c7c3856eab6b8ab649c7ed43d72609bf0e0b1d6d
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-22 23:13:18 +0530

    feat: creating an editor

--- components/Editor/index.tsx
+++ components/Editor/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import styles from "./index.module.css";
-const Editor=()=>{
-    return <div className={styles.editor}>Editor Component</div>
+const Editor = () => {
+
+    const [javaScriptCode, setJavaScriptCode] = useState("");
+    return <textarea className={styles.editor} value={javaScriptCode} onChange={(e) => setJavaScriptCode(e.target.value)} 
+    placeholder="Write your JavaScript code here..." />;
 }
 
 export default Editor;
