981aaa98581e7f4f645c5f50a90eead638fdc9df
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-23 10:27:46 +0530

    making header responsive

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useWindowSize } from "../../hooks/isDesktop";
 import styles from "./index.module.css"
 
 const Header=()=>{
@@ -5,14 +6,15 @@ const Header=()=>{
     const redirectToLinkedin=()=>{
         window.open("https://www.linkedin.com/in/aditya-sharma-693244171/","_blank")
     }
-return (<div className={styles.header}>
+    const {isDesktop}=useWindowSize();
+return (<div className={isDesktop?styles.header: styles.headerMobile}>
     <div className={styles.title}>
-        <h2 className={styles.titleText} >JavaScript & TypeScript Playground</h2>
+        <h2 className={isDesktop?styles.titleText : styles.titleTextMobile} >JavaScript & TypeScript Playground</h2>
     </div>
-    <div className={styles.subtitle}>
+    {isDesktop && <div className={styles.subtitle}>
         <span className={styles.subtitleText1}>Code • Run • Debug</span>
         <span className={styles.subtitleText2} onClick={redirectToLinkedin}>By Aditya Sharma</span>
-    </div>
+    </div>}
 </div>)
 }
 
