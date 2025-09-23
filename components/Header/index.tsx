d75672f6fb562e4a80c2218320096a5fea556782
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-23 11:08:35 +0530

    adding dynamic routing

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,28 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { useWindowSize } from "../../hooks/isDesktop";
 import styles from "./index.module.css"
+import { useEffect, useState } from "react";
 
 const Header=()=>{
+    const {isDesktop}=useWindowSize();
+    const[title,setTitle]=useState("JavaScript Playground");
+    const pathname = usePathname();
+    useEffect(()=>{
+        if(pathname.includes("typescript")){
+            setTitle("TypeScript Playground");
+        }else{
+            setTitle("JavaScript Playground");
+        }
+    },[pathname])
 
     const redirectToLinkedin=()=>{
         window.open("https://www.linkedin.com/in/aditya-sharma-693244171/","_blank")
     }
-    const {isDesktop}=useWindowSize();
+    
 return (<div className={isDesktop?styles.header: styles.headerMobile}>
     <div className={styles.title}>
-        <h2 className={isDesktop?styles.titleText : styles.titleTextMobile} >JavaScript & TypeScript Playground</h2>
+        <h2 className={isDesktop?styles.titleText : styles.titleTextMobile} >{title}</h2>
     </div>
     {isDesktop && <div className={styles.subtitle}>
         <span className={styles.subtitleText1}>Code • Run • Debug</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
-import Editor from "../../components/Editor";
-import OutputScreen from "../../components/OutputScreen";
-import styles from "./page.module.css";
-import { useWindowSize } from "../../hooks/isDesktop";
-import MobileHeader from "../../components/MobileHeader";
-import Header from "../../components/Header";
-import { placeHolderText } from "../../components/Editor/const";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 
 export default function Home() {
-  const [output, setOutput] = useState("");
-  const [input, setInput] = useState(placeHolderText);
-  const { isDesktop } = useWindowSize();
-  const [selectedTab, setSelectedTab] = useState<"input" | "output">("input");
-
-  useEffect(() => { if (!isDesktop) setSelectedTab("output") }, [output]);
-  return (
-    <div className={styles.main_container}>
-     <Header/>
-      {isDesktop ? <div className={styles.page}>
-        <Editor setOutput={setOutput} javaScriptCode={input} setJavaScriptCode={setInput} />
-        <OutputScreen output={output} setOutput={setOutput} />
-      </div> :
-        <div className={styles.mobile_page} >
-          <MobileHeader selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-          {selectedTab === "input" && <Editor setOutput={setOutput} javaScriptCode={input} setJavaScriptCode={setInput} />}
-          {selectedTab === "output" && <OutputScreen output={output} setOutput={setOutput} />}
-        </div>
-      }
-    </div>
-
-  );
+    const router = useRouter();
+    const redirect = () => {
+        router.push("/javascript");
+    }
+    useEffect(() => { redirect() }, []);
+    return (
+        <></>
+    );
 }
